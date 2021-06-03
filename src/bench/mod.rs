40837d6b51e66826828f2fcda4df9f581aa15810
OperatorGK <operator.gk@gmail.com>
2021-06-03 22:14:26 +0300

    more benches

--- src/bench/mod.rs
+++ src/bench/mod.rs
@@ -11,32 +11,79 @@ mod bench {
 
     use super::test::{black_box, Bencher};
 
+    const BENCH_SIZE: usize = 155;
+
+    #[bench]
+    fn bench_rng(b: &mut Bencher) {
+        b.iter(|| {
+            let a = Array::random([BENCH_SIZE, BENCH_SIZE], Uniform::new(-10., 10.));
+            black_box(&a);
+        })
+    }
+
+    #[bench]
+    fn bench_qr(b: &mut Bencher) {
+        b.iter(|| {
+            let a = Array::random([BENCH_SIZE, BENCH_SIZE], Uniform::new(-10., 10.));
+            let (q, _) = qr_decomposition(a.view()).unwrap();
+            black_box(&q);
+        })
+    }
+
+    #[bench]
+    fn bench_hess(b: &mut Bencher) {
+        b.iter(|| {
+            let a = Array::random([BENCH_SIZE, BENCH_SIZE], Uniform::new(-10., 10.));
+            let (h, _) = hessenberg_form(a.view()).unwrap();
+            black_box(&h);
+        })
+    }
+
     #[bench]
     fn bench_francis(b: &mut Bencher) {
-        let a = Array::random([50, 50], Uniform::new(-10., 10.));
         b.iter(|| {
+            let a = Array::random([BENCH_SIZE, BENCH_SIZE], Uniform::new(-10., 10.));
             let (t, _) = schur_form(a.view()).unwrap();
-            black_box(t);
+            black_box(&t);
         })
     }
 
     #[bench]
     fn bench_symmetric(b: &mut Bencher) {
-        let mut a = Array::random([50, 50], Uniform::new(-10., 10.));
-        for i in 0..50 {
-            for j in 0..i {
-                a[[i, j]] = a[[j, i]]
-            }
-        }
-
         let opts = QROptions {
             algorithm: QRAlgorithm::Symmetric,
             ..DEFAULT_OPTS
         };
 
         b.iter(|| {
+            let mut a = Array::random([BENCH_SIZE, BENCH_SIZE], Uniform::new(-10., 10.));
+            for i in 0..BENCH_SIZE {
+                for j in 0..i {
+                    a[[i, j]] = a[[j, i]]
+                }
+            }
+
             let (t, _) = schur_form_opts(a.view(), &opts).unwrap();
-            black_box(t);
+            black_box(&t);
         })
+    }
+
+    #[bench]
+    fn bench_svd(b: &mut Bencher) {
+        b.iter(|| {
+        let a = Array::random([BENCH_SIZE, BENCH_SIZE], Uniform::new(-10., 10.));
+            let (_, s, _) = svd(a.view()).unwrap();
+            black_box(&s);
+        })
+    }
+
+    #[bench]
+    fn bench_eigenvalues(b: &mut Bencher) {
+        b.iter(|| {
+            let a = Array::random([BENCH_SIZE, BENCH_SIZE], Uniform::new(-10., 10.));
+            let e = eigenvalues(a.view()).unwrap();
+            black_box(&e);
+        }
+        )
     }
 }
